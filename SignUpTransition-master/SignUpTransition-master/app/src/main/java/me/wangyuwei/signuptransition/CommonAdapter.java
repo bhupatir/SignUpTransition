20a0b48284169cbb5e148b53154f5cc862aba60d
bhupatir <bhupati@srivatech.com>
2016-08-26 14:46:07 +0530

    Initial commit

---
+++ SignUpTransition-master/SignUpTransition-master/app/src/main/java/me/wangyuwei/signuptransition/CommonAdapter.java
@@ -0,0 +1,39 @@
+package me.wangyuwei.signuptransition;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CommonAdapter extends RecyclerView.Adapter<CommonAdapter.CommonViewHolder> {
+
+    private final LayoutInflater mLayoutInflater;
+
+    public CommonAdapter(Context context) {
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public CommonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new CommonViewHolder(mLayoutInflater.inflate(R.layout.rv_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(CommonViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 100;
+    }
+
+    public static class CommonViewHolder extends RecyclerView.ViewHolder {
+
+        public CommonViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ SignUpTransition-master/SignUpTransition-master/app/src/main/java/me/wangyuwei/signuptransition/DensityUtil.java
@@ -0,0 +1,20 @@
+package me.wangyuwei.signuptransition;
+
+import android.content.Context;
+
+
+public class DensityUtil {
+
+    private DensityUtil(){}
+
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+}
